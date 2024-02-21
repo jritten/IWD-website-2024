f087ecb09a11eec9fb1557ae96dd112735134834
maryamtello1 <maryamt@umich.edu>
2024-02-21 16:06:07 -0500

    create hero component

---
+++ iwd-app/src/components/hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/hero.css"
+
+function Hero() {
+    return (
+        <div className="hero-banner">
+            {/* hero content goes here */}
+        </div>
+    );
+}
+
+export default Hero;
