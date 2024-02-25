e606fdc5805e118f743317bcea72d0d87025e367
moe <msalma@umich.edu>
2024-02-24 23:23:46 -0500

    implemented the scroll functionality to the Navbar

--- iwd-app/src/components/navbar/navbar.jsx
+++ iwd-app/src/components/navbar/navbar.jsx
@@ -1,20 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
+
 function NavBar() {
+  const [activeItem, setActiveItem] = useState(null);
+
+  const scrollHandler = (className) => {// responsible for the scrolling 
+    const element = document.querySelector(`.${className}`);
+    
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'center', inline:'start'});
+      setActiveItem(className);{/* if use hovers over a section set it as active section */}
+      
+    }
+  };
+
   return (
-    //<div className="navbar">{/* navbar content goes here */}I  NavBar</div>
+    
     <nav className="Nav">
-    <ul>
-      <li><a href="#speakers">Speakers</a></li>
-      <li><a href="#sessions">Sessions</a></li>
-      <li><a href="#location">Location</a></li>
-      <li><a href="#sponsors">Sponsors</a></li>
-      <li><a href="#organizers">Organizers</a></li>
-      <li><a href="#facilitators">Facilitators</a></li>
-      <li><a href="#devteam">Dev Team</a></li>
-    </ul>
-  </nav>
-  
+      {/*used class-name of each "div"/component to reference it in the navbar scrolling functionality*/}
+      {/*this part will allow scrolling as well as check which section the user is currently hovering over or clicked*/}
+      <ul> 
+
+        <li className={activeItem === 'speakers-div' ? 'active' : ''}>
+          <a onClick={() => scrollHandler('speakers-div')}>Speakers</a>
+        </li>
+
+        <li className={activeItem === 'sessions-div' ? 'active' : ''}>
+          <a onClick={() => scrollHandler('sessions-div')}>Sessions</a>
+        </li>
+
+        <li className={activeItem === 'location-div' ? 'active' : ''}>
+          <a onClick={() => scrollHandler('location-div')}>location</a>
+        </li>
+
+        <li className={activeItem === 'sponsors-div' ? 'active' : ''}>
+          <a onClick={() => scrollHandler('sponsors-div')}>Sponsors</a>
+        </li>
+
+        <li className={activeItem === 'organizers-div' ? 'active' : ''}>
+          <a onClick={() => scrollHandler('organizers-div')}>organizers</a>
+        </li>
+
+        <li className={activeItem === 'facilitators-div' ? 'active' : ''}>
+          <a onClick={() => scrollHandler('facilitators-div')}>Facilitators</a>
+        </li>
+
+        <li className={activeItem === 'devTeam-div' ? 'active' : ''}>
+          <a onClick={() => scrollHandler('devTeam-div')}>Dev Team</a>
+        </li>
+
+      </ul>
+    </nav>
   );
 }
 
