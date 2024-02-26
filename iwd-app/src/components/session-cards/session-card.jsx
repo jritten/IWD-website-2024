ed6ba75f829dfc1a76aa8fd4da4567aab3eecb14
Heba Sayed <hebasayed1603@gmail.com>
2024-02-26 00:07:25 -0500

    Finished overall structure and styling of card.

--- iwd-app/src/components/session-cards/session-card.jsx
+++ iwd-app/src/components/session-cards/session-card.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from "react";
+import "./session-card.css";
 
-function SessionCard() {
+function SessionCard({ title, imageUrl, speaker, time, place }) {
   return (
     <div className="session-card">
-      {/* session card content goes here */}
+      <div className="imageContainer">
+        <img src={imageUrl} alt="Person" />
+      </div>
+      <div className="infoContainer">
+        <div className="title">{title}</div>
+        <div className="speaker">By {speaker}</div>
+        <div className="time">
+          From <span>{time}</span> in {place}
+        </div>
+      </div>
+      <div className="arrowContainer">
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+        />
+
+        <span
+          className="material-symbols-outlined"
+          style={{ fontSize: "50px" }}
+        >
+          expand_circle_down
+        </span>
+      </div>
     </div>
   );
 }
