c7c3ad146122bd9195d7ca463fd08ae2e604c8f9
khanabdu25 <khanabdu@umich.edu>
2024-02-26 17:34:25 -0500

    testing in page

--- iwd-app/src/components/sessions-section/sessions.jsx
+++ iwd-app/src/components/sessions-section/sessions.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./sessions.css";
+import SessionCard from "../session-cards/session-card";
 
 function Sessions() {
   return (
     <div className="sessions-div">
       {/* Session div content goes here */}I am Sessions
+      <div className="sessions-row">
+        <SessionCard
+          title="Introduction to React Hooks"
+          imageUrl="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
+          speaker="John Doe"
+          time="10:00 AM - 11:30 AM"
+          place="Main Hall"
+          description="Lorem "
+        />
+      </div>
     </div>
   );
 }
