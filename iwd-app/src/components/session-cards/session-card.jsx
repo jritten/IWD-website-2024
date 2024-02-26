34adb4c3e2ba7d3c8a94a217887a14f2fb3a9a1f
Heba Sayed <hebasayed1603@gmail.com>
2024-02-26 00:58:01 -0500

    added expand/collapse function and fixed img css

--- iwd-app/src/components/session-cards/session-card.jsx
+++ iwd-app/src/components/session-cards/session-card.jsx
@@ -1,32 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./session-card.css";
 
-function SessionCard({ title, imageUrl, speaker, time, place }) {
+function SessionCard({ title, imageUrl, speaker, time, place, description }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
-    <div className="session-card">
-      <div className="imageContainer">
-        <img src={imageUrl} alt="Person" />
-      </div>
-      <div className="infoContainer">
-        <div className="title">{title}</div>
-        <div className="speaker">By {speaker}</div>
-        <div className="time">
-          From <span>{time}</span> in {place}
+    <div className={`session-card ${isExpanded ? "expanded" : ""}`}>
+      <div className="main-content">
+        <div className="imageContainer">
+          <img src={imageUrl} alt="Person" />
+        </div>
+        <div className="infoContainer">
+          <div className="title">{title}</div>
+          <div className="speaker">By {speaker}</div>
+          <div className="time">
+            From <span>{time}</span> in {place}
+          </div>
+        </div>
+        <div
+          className={`arrowContainer ${isExpanded ? "expanded" : ""}`}
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+          />
+          <span
+            className={`material-symbols-outlined ${
+              isExpanded ? "rotate" : ""
+            }`}
+            style={{ fontSize: "50px" }}
+          >
+            expand_circle_down
+          </span>
         </div>
       </div>
-      <div className="arrowContainer">
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
-        />
-
-        <span
-          className="material-symbols-outlined"
-          style={{ fontSize: "50px" }}
-        >
-          expand_circle_down
-        </span>
-      </div>
+      {isExpanded && (
+        <div className="description-container">
+          <div className="description">{description}</div>
+        </div>
+      )}
     </div>
   );
 }
