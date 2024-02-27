0affe0b1fe98368cc88bedfb8bf087bbfe6bca5a
Mohamad Abboud <mohamadabboud200@outlook.com>
2024-02-27 18:30:16 -0500

    updated SpeakerCard component

--- iwd-app/src/components/speaker-cards/speaker-card.jsx
+++ iwd-app/src/components/speaker-cards/speaker-card.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import "./speaker-card.css";
 
-function SpeakerCard() {
+function SpeakerCard(imageurl, name, position, NA, description) {
   return (
     <div className="speaker-card">
-      {/* Speaker card content goes here */}
+       <div className="speaker-image">
+          <img src={imageurl} alt="Speaker image"/>
+       </div>
+       <div className='speaker-name'>
+          Martin Jacobs
+       </div>
+       <div className='speaker-position'>
+          UX Designer
+       </div>
+       <div className='speaker-NA'>
+          Rocket Homes
+       </div>
+       <div className='speaker-description'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis ut lectus non sollicitudin. 
+       </div>
     </div>
   );
 }
