2d87018a7483ca5fe6a9bfb439f229ece370db61
Mohamad Abboud <mohamadabboud200@outlook.com>
2024-02-27 18:32:30 -0500

    testing the speaker cards

--- iwd-app/src/components/speakers-section/speakers.jsx
+++ iwd-app/src/components/speakers-section/speakers.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./speakers.css";
+import SpeakerCard from "../speaker-cards/speaker-card";
 
 function Speakers() {
   return (
     <div className="speakers-div">
       {/* Speaker div content goes here */}I am Speakers
+      <div className="speaker-cards-container">
+        <SpeakerCard/>
+        <SpeakerCard/>
+        <SpeakerCard/>
+      </div>
     </div>
   );
 }
