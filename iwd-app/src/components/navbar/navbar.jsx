afc3ee373d3af82749b925f0fbd633ad380276d6
khanabdu25 <khanabdu@umich.edu>
2024-02-28 15:47:28 -0500

    so far

--- iwd-app/src/components/navbar/navbar.jsx
+++ iwd-app/src/components/navbar/navbar.jsx
@@ -76,41 +76,37 @@ function NavBar() {
         </ul>
       </nav>
       {open && (
-        <div className="drawer">
-          {/* the following is copy-pasted, the only difference is that this list has the class name hamburger-list,
-            and thats how its modified in the css, instead of Nav li or Nav ul for the main header list */}
-          <ul className="hamburger-list">
-            <li className={activeItem === "speakers-div" ? "active" : ""}>
-              <a onClick={() => scrollHandler("speakers-div")}>Speakers</a>
-            </li>
+        <ul className="hamburger-list">
+          <li className={activeItem === "speakers-div" ? "active" : ""}>
+            <a onClick={() => scrollHandler("speakers-div")}>Speakers</a>
+          </li>
 
-            <li className={activeItem === "sessions-div" ? "active" : ""}>
-              <a onClick={() => scrollHandler("sessions-div")}>Sessions</a>
-            </li>
+          <li className={activeItem === "sessions-div" ? "active" : ""}>
+            <a onClick={() => scrollHandler("sessions-div")}>Sessions</a>
+          </li>
 
-            <li className={activeItem === "location-div" ? "active" : ""}>
-              <a onClick={() => scrollHandler("location-div")}>Location</a>
-            </li>
+          <li className={activeItem === "location-div" ? "active" : ""}>
+            <a onClick={() => scrollHandler("location-div")}>Location</a>
+          </li>
 
-            <li className={activeItem === "sponsors-div" ? "active" : ""}>
-              <a onClick={() => scrollHandler("sponsors-div")}>Sponsors</a>
-            </li>
+          <li className={activeItem === "sponsors-div" ? "active" : ""}>
+            <a onClick={() => scrollHandler("sponsors-div")}>Sponsors</a>
+          </li>
 
-            <li className={activeItem === "organizers-div" ? "active" : ""}>
-              <a onClick={() => scrollHandler("organizers-div")}>Organizers</a>
-            </li>
+          <li className={activeItem === "organizers-div" ? "active" : ""}>
+            <a onClick={() => scrollHandler("organizers-div")}>Organizers</a>
+          </li>
 
-            <li className={activeItem === "facilitators-div" ? "active" : ""}>
-              <a onClick={() => scrollHandler("facilitators-div")}>
-                Facilitators
-              </a>
-            </li>
+          <li className={activeItem === "facilitators-div" ? "active" : ""}>
+            <a onClick={() => scrollHandler("facilitators-div")}>
+              Facilitators
+            </a>
+          </li>
 
-            <li className={activeItem === "devTeam-div" ? "active" : ""}>
-              <a onClick={() => scrollHandler("devTeam-div")}>Dev Team</a>
-            </li>
-          </ul>
-        </div>
+          <li className={activeItem === "devTeam-div" ? "active" : ""}>
+            <a onClick={() => scrollHandler("devTeam-div")}>Dev Team</a>
+          </li>
+        </ul>
       )}
     </div>
   );
