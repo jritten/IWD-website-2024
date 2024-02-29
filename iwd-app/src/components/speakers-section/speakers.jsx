8919ef016b3d2d6898cb0a34e8ac38523acc6796
Mohamad Abboud <mohamadabboud200@outlook.com>
2024-02-28 19:20:41 -0500

    speakers section completed with speakercards

--- iwd-app/src/components/speakers-section/speakers.jsx
+++ iwd-app/src/components/speakers-section/speakers.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import "./speakers.css";
 import SpeakerCard from "../speaker-cards/speaker-card";
+import speakersData from "./speakers-list.js"
 
 function Speakers() {
+
   return (
     <div className="speakers-div">
       {/* Speaker div content goes here */}I am Speakers
       <div className="speaker-cards-container">
-        <SpeakerCard/>
-        <SpeakerCard/>
-        <SpeakerCard/>
-        <SpeakerCard/>
-        <SpeakerCard/>
-        <SpeakerCard/>
+        {speakersData.map((speaker, index) => (
+          <SpeakerCard 
+            key={index}
+            imageurl={speaker.imageurl}
+            name={speaker.name}
+            position={speaker.position}
+            company={speaker.company}
+            description={speaker.description}
+          />
+        ))}
       </div>
     </div>
   );
