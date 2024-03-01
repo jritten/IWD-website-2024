0c639256e881d97ea89d5f670d6374a099d93b04
maryamtello1 <maryamt@umich.edu>
2024-03-01 02:34:24 -0500

    added background asset to hero

--- iwd-app/src/components/hero-banner/hero.jsx
+++ iwd-app/src/components/hero-banner/hero.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./hero.css";
 import globeGIF from "../../assets/hero/globe.gif"
 import heroGirl from "../../assets/hero/women-hero.png";
+import background from "../../assets/hero/hero-background.PNG";
+
 
 
 function Hero() {
@@ -20,14 +22,13 @@ function Hero() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum
+            aliquip ex ea commodo consequat. 
           </p>
+          <btn id="tickets-btn">Namaa's button</btn>
         </div>
         <img id="woman-hero" src={heroGirl}></img>
       </div>
+      <img id="background-img" src={background}></img>
     </div>
   );
 }
