a5a1b50b58179f4e8ec646cf48ac28a50c52a550
maryamtello1 <maryamt@umich.edu>
2024-03-01 01:45:55 -0500

    added woman image to hero

--- iwd-app/src/components/hero-banner/hero.jsx
+++ iwd-app/src/components/hero-banner/hero.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import "./hero.css";
 import globeGIF from "../../assets/hero/globe.gif"
+import heroGirl from "../../assets/hero/women-hero.png";
+
 
 function Hero() {
   return (
     <div className="hero-banner">
-      <div className="title-div">
-        <div className="heading-div">
-          <h1>Impact <br /> the Future</h1>
-          <img id="globe-gif" src={globeGIF}></img>
+      <div className="top-row">
+        <div className="title-div">
+          <div className="heading-div">
+            <h1>
+              Impact <br /> the Future
+            </h1>
+            <img id="globe-gif" src={globeGIF}></img>
+          </div>
+          <h2>International Women's Day 2024</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
         </div>
-        <h2>International Women's Day 2024</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
-        </p>
+        <img id="woman-hero" src={heroGirl}></img>
       </div>
     </div>
   );
