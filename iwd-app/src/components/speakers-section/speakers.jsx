84cdd5dd8ecf051fe4ef6b5d58b366c9594742de
Mohamad Abboud <mohamadabboud200@outlook.com>
2024-03-02 17:12:09 -0500

    added speakers header

--- iwd-app/src/components/speakers-section/speakers.jsx
+++ iwd-app/src/components/speakers-section/speakers.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import "./speakers.css";
 import SpeakerCard from "../speaker-cards/speaker-card";
 import speakersData from "./speakers-list.js"
+import underlineImg from "../../assets/Underline.png";
+
 
 function Speakers() {
 
   return (
     <div className="speakers-div">
+      <div className="speakers-header">
+        Speakers
+      </div>
+      <img src={underlineImg} alt="" className="underline2"/>
       <div className="speaker-cards-container">
         {speakersData.map((speaker, index) => (
           <SpeakerCard key={index} {...speaker}/>
