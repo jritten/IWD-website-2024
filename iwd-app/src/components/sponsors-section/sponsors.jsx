f508ecd3f9b654b5db905479140dfbefbb6c8772
Whitney <trasua@umich.edu>
2024-03-02 19:43:46 -0500

    Added section for flex display in css

--- iwd-app/src/components/sponsors-section/sponsors.jsx
+++ iwd-app/src/components/sponsors-section/sponsors.jsx
@@ -5,7 +5,7 @@ import "./sponsors.css";
 function Sponsors() {
   return (
     <div className="sponsors-div">
-    <div class="title">
+    <div class="header-container">
       <div class = "title">Partners</div>
       <link href="https://fonts.googleapis.com/css?family=Rock+Salt:regular" rel="stylesheet" />
       <img src= "https://drive.google.com/thumbnail?id=1WRP5oPKnIi0ukt9V5I1QO586ONusla8r" alt="" class="underline" />
