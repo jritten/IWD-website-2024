0396d5c1686b7f1f045c15aba9c8acab0136aff9
Whitney <trasua@umich.edu>
2024-03-02 19:19:09 -0500

    Added title and underline

--- iwd-app/src/components/speakers-section/speakers.jsx
+++ iwd-app/src/components/speakers-section/speakers.jsx
@@ -4,9 +4,14 @@ import "./speakers.css";
 function Speakers() {
   return (
     <div className="speakers-div">
-      {/* Speaker div content goes here */}I am Speakers
-    </div>
+      <div class="container">
+      <div class="title">Speakers </div>
+    <link href="https://fonts.googleapis.com/css?family=Rock+Salt:regular" rel="stylesheet" />
+    <img src= "https://drive.google.com/thumbnail?id=1WRP5oPKnIi0ukt9V5I1QO586ONusla8r" alt="" class="underline" /> 
+  </div>
+  </div>
   );
 }
 
 export default Speakers;
+
