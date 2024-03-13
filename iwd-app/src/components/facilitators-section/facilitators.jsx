4870e1abf0ba9beb740c0b634ae4a03d80c2a761
Namaa Mokhtar <nmokh05@gmail.com>
2024-03-13 11:59:03 -0400

    button and facilitators

--- iwd-app/src/components/facilitators-section/facilitators.jsx
+++ iwd-app/src/components/facilitators-section/facilitators.jsx
@@ -5,6 +5,7 @@ function Facilitators() {
   return (
     /* app-section style is necessary, please do not delete */
     <div className="app-section facilitators-div">
+        <div className="facilitators-header">Facilitators</div>
       {/* Facilitators div content goes here */}I am Facilitators
     </div>
   );
