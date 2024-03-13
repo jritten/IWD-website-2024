cfc9f1b0edc813d76bce05118f87d0841e02253e
Mohamad Abboud <mohamadabboud200@outlook.com>
2024-03-12 23:52:17 -0400

    Created Organizer card component jsx file

---
+++ iwd-app/src/components/organizer-cards/organizer-cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './organizer-card.css'
+
+function OrganizerCard() {
+    return (
+        <div className='organizer-card'>
+
+
+
+        </div>
+    );
+}
+
+export default OrganizerCard;
