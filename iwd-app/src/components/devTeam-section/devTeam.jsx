81cf80ab3f22299b9ba3d8eaaf365f143b616b35
maryamtello1 <maryamt@umich.edu>
2024-03-13 12:25:04 -0400

    removed "I am dev team"

--- iwd-app/src/components/devTeam-section/devTeam.jsx
+++ iwd-app/src/components/devTeam-section/devTeam.jsx
@@ -3,9 +3,7 @@ import "./devTeam.css";
 
 function DevTeam() {
   return (
-    /* app-section style is necessary, please do not delete */
     <div className="app-section devTeam-div">
-      {/* Dev Team div content goes here */}I am Dev Team
     </div>
   );
 }
