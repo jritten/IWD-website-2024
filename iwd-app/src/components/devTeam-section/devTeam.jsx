c88d59cfd35dccffbd8a5a343bdc517fd7e59938
maryamtello1 <maryamt@umich.edu>
2024-03-13 15:02:10 -0400

    added global styles & header to devteam

--- iwd-app/src/components/devTeam-section/devTeam.jsx
+++ iwd-app/src/components/devTeam-section/devTeam.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./devTeam.css";
+import "../../globalStyles.css";
+
 
 function DevTeam() {
   return (
     <div className="app-section devTeam-div">
+      <div className="section-title">Dev Team</div>
     </div>
   );
 }
