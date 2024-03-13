958b77ebe5770ed1d88258fa85435e741695834b
maryamtello1 <maryamt@umich.edu>
2024-03-13 15:02:43 -0400

    added global styles to speakers

--- iwd-app/src/components/speakers-section/speakers.jsx
+++ iwd-app/src/components/speakers-section/speakers.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./speakers.css";
+import "../../globalStyles.css";
+
 import SpeakerCard from "../speaker-cards/speaker-card";
 import speakersData from "./speakers-list.js";
 
@@ -7,7 +9,7 @@ function Speakers() {
   return (
     /* app-section style is necessary, please do not delete */
     <div className="app-section speakers-div">
-      <div className="speakers-header">Speakers</div>
+      <div className="section-title">Speakers</div>
       <div className="speaker-cards-container">
         {speakersData.map((speaker, index) => (
           <SpeakerCard key={index} {...speaker} />
