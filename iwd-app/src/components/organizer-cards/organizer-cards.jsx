b5f1ce5d2f7d4245a769865ed4ba85891e14c939
Mohamad Abboud <mohamadabboud200@outlook.com>
2024-03-13 00:55:49 -0400

    created organizer-cards

--- iwd-app/src/components/organizer-cards/organizer-cards.jsx
+++ iwd-app/src/components/organizer-cards/organizer-cards.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './organizer-card.css'
 
-function OrganizerCard() {
+function OrganizerCard({imageurl, name, position}) {
     return (
         <div className='organizer-card'>
-
-
-
+            <div className="organizer-image">
+                <img src={imageurl = "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg"} alt="Organizer image"/>
+            </div>
+            <div className='organizer-name'>
+                {name || "Rocket Homes"}
+            </div>
+            <div className='organizer-position'>
+                {position || "Senior IOS Developer"}
+            </div>
         </div>
     );
 }
