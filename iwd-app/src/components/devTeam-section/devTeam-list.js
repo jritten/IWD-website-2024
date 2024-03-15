27dfb36bab7e02edb93d8ec589f4ceddb438e8fb
Mohamad Abboud <mohamadabboud200@outlook.com>
2024-03-15 01:47:47 -0400

    updated datalist

--- iwd-app/src/components/devTeam-section/devTeam-list.js
+++ iwd-app/src/components/devTeam-section/devTeam-list.js
@@ -2,62 +2,62 @@ const devTeamData = [
     {
         imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
         name: "",
-        position: "",
+        linkedIn: "https://www.linkedin.com/in/moeabboud/",
     },
     {
         imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
         name: "",
-        position: "",
+        linkedIn: "https://www.linkedin.com/in/moeabboud/",
     },
     {
         imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
         name: "",
-        position: "",
+        linkedIn: "https://www.linkedin.com/in/moeabboud/",
     },
     {
         imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
         name: "",
-        position: "",
+        linkedIn: "https://www.linkedin.com/in/moeabboud/",
     },
     {
         imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
         name: "",
-        position: "",
+        linkedIn: "https://www.linkedin.com/in/moeabboud/",
     },
     {
         imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
         name: "",
-        position: "",
+        linkedIn: "https://www.linkedin.com/in/moeabboud/",
     },
     {
         imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
         name: "",
-        position: "",
+        linkedIn: "https://www.linkedin.com/in/moeabboud/",
     },
     {
         imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
         name: "",
-        position: "",
+        linkedIn: "https://www.linkedin.com/in/moeabboud/",
     },
     {
         imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
         name: "",
-        position: "",
+        linkedIn: "https://www.linkedin.com/in/moeabboud/",
     },
     {
         imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
         name: "",
-        position: "",
+        linkedIn: "https://www.linkedin.com/in/moeabboud/",
     },
     {
         imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
         name: "",
-        position: "",
+        linkedIn: "https://www.linkedin.com/in/moeabboud/",
     },
     {
         imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
         name: "",
-        position: "",
+        linkedIn: "https://www.linkedin.com/in/moeabboud/",
     },
 
 
