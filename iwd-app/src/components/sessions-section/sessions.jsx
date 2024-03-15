36487128a324ee6a52a23b8873c142a133315496
moe <msalma@umich.edu>
2024-03-14 23:57:42 -0400

    migrating to tailwind

--- iwd-app/src/components/sessions-section/sessions.jsx
+++ iwd-app/src/components/sessions-section/sessions.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./sessions.css";
+//import "./sessions.css";
 import SessionCard from "../session-cards/session-card";
 import networkingCards from "./content-list";
 import talkSeminarsCards from "./content-list2";
@@ -12,28 +12,28 @@ function Sessions() {
   };
 
   return (
-    <div className="app-section sessions-div">
-      <div className="header-container">
-        <div className="sessions-title">Sessions</div>
+    <div className="app-section sessions-div w-full font bg-[#d1ece3] flex flex-col gap-0 items-center p-5">
+      <div className="header-container flex">
+        <div className="sessions-title text-3xl font-bold text-center w-full h-[152px] font-session text-[40px] leading-[95.43px]">Sessions</div>
       </div>
-      <div className="tabs-container">
+      <div className="tabs-container flex">
         <div
-          className={`tab ${activeTab === "Networking" ? "active" : ""}`}
+          className={`tab ${activeTab === "Networking" ? "active" : ""} flex bg-[#2480f0] rounded-lg cursor-pointer text-center  duration-[0.5s] mr-10 px-[px] py-5 border-b-[none] border-2 border-solid border-black first:rounded-tl-[20px] `}
           onClick={() => handleTabClick("Networking")}
         >
-          <span>Networking/Social Events</span>
+          <span >Networking/Social Events</span>
         </div>
         <div
-          className={`tab ${activeTab === "Talks/Seminars" ? "active" : ""}`}
+          className={`tab ${activeTab === "Talks/Seminars" ? "active " : ""} w-[1000px] cursor-pointer bg-white text-center transition-[background-color] duration-[0.5s] mr-0 px-[15px] py-5 border-b-[none] border-2 border-solid border-black first:rounded-tl-[20px] first:border-r-[none]`}
           onClick={() => handleTabClick("Talks/Seminars")}
         >
-          <span>Talks/Seminars</span>
+          <span className="scale-110 inline-block align-middle text-lg font-medium transition-transform duration-[0.3s]">Talks/Seminars</span>
         </div>
       </div>
-      <div className="session-cards-container">
+      <div className="session-cards-container flex flex-col items-stretch gap-5 max-w-[1000px] w-[90%] bg-[white] mx-auto my-0 rounded-br-[20px] rounded-bl-[20px] border-2 border-solid border-[black]">
         {activeTab === "Networking" &&
           networkingCards.map((session, index) => (
-            <div className="sessions-row" key={index}>
+            <div className="sessions-row flex flex-col items-center gap-5" key={index}>
               <SessionCard
                 title={session.title}
                 imageUrl={session.imageUrl}
@@ -47,9 +47,10 @@ function Sessions() {
               />
             </div>
           ))}
+          </div>
         {activeTab === "Talks/Seminars" &&
           talkSeminarsCards.map((session, index) => (
-            <div className="sessions-row" key={index}>
+            <div className="sessions-row flex flex-col items-center gap-5" key={index}>
               <SessionCard
                 title={session.title}
                 imageUrl={session.imageUrl}
@@ -63,7 +64,7 @@ function Sessions() {
               />
             </div>
           ))}
-      </div>
+      
     </div>
   );
 }
