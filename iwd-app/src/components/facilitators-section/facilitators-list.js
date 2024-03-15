8775158e45d3bfcb23ffd23715e8ed98d530b7ca
Mohamad Abboud <mohamadabboud200@outlook.com>
2024-03-14 23:02:38 -0400

    facilitators data list file created and populated

---
+++ iwd-app/src/components/facilitators-section/facilitators-list.js
@@ -0,0 +1,55 @@
+const facilitatorsData = [
+    {
+        imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
+        name: "",
+        position: "",
+    },
+    {
+        imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
+        name: "",
+        position: "",
+    },
+    {
+        imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
+        name: "",
+        position: "",
+    },
+    {
+        imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
+        name: "",
+        position: "",
+    },
+    {
+        imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
+        name: "",
+        position: "",
+    },
+    {
+        imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
+        name: "",
+        position: "",
+    },
+    {
+        imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
+        name: "",
+        position: "",
+    },
+    {
+        imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
+        name: "",
+        position: "",
+    },
+    {
+        imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
+        name: "",
+        position: "",
+    },
+    {
+        imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
+        name: "",
+        position: "",
+    },
+
+]
+
+export default facilitatorsData;
