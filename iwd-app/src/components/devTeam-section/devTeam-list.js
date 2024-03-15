95c00b2ceb0d0a0db2cd6e3b421b0ea15a222be5
Mohamad Abboud <mohamadabboud200@outlook.com>
2024-03-15 15:56:34 -0400

    integrated photos into devTeam data

--- iwd-app/src/components/devTeam-section/devTeam-list.js
+++ iwd-app/src/components/devTeam-section/devTeam-list.js
@@ -1,13 +1,16 @@
+import MoeAbboud from "../../assets/headshots/MoeAbboud.jpg"
+import MoeChami from "../../assets/headshots/MoeChami.jpg"
+
 const devTeamData = [
     {
-        imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
-        name: "",
+        imageurl: MoeAbboud,
+        name: "Mohamad Abboud",
         linkedIn: "https://www.linkedin.com/in/moeabboud/",
     },
     {
-        imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
-        name: "",
-        linkedIn: "https://www.linkedin.com/in/moeabboud/",
+        imageurl: MoeChami,
+        name: "Mohammad El-Chami",
+        linkedIn: "https://www.linkedin.com/in/moecham/",
     },
     {
         imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
