f5b5bf967b05a9528733d4790570c4cc09b9d969
maryamtello1 <maryamt@umich.edu>
2024-03-16 23:53:19 -0400

    added updates sessions card content

--- iwd-app/src/components/sessions-section/content-list2.js
+++ iwd-app/src/components/sessions-section/content-list2.js
@@ -1,83 +1,59 @@
 const talkSeminarsCards = [
   {
-    title: "Welcome & Intros",
+    title: "Lightning Talk #1 - Tech, Design & AI",
+    imageUrl: "https://i.pravatar.cc/150?img=50",
+    time: "9:30 AM",
+    place: "Conference Room A",
+    description:
+      "Join Seungju Choi from the University of Michigan's Human System Integration Lab for an insightful discussion on drivers' performance, distraction, and satisfaction.",
+    type: "regular",
+    speaker:
+      "Seungju Choi @ University of Michigan, Human System Integration Lab",
+  },
+  {
+    title: "Lightning Talk #2 - Detroit Artistry",
     imageUrl: "https://i.pravatar.cc/150?img=50",
     time: "10:00 AM",
-    place: "Main Hall",
+    place: "Conference Room B",
     description:
-      "Get introduced to the event and meet the organizers from NSBE, SHPE, and SWE Detroit Professional Chapters!",
+      "Immerse yourself in the soulful creativity of Detroit. Discover the heartbeat of a city known for its vibrant cultural tapestry and artistic flair.",
     type: "regular",
-    speaker: "Michael Johnson",
+    speaker: "TBD",
   },
   {
-    title: "Lightning Talk #1",
+    title:
+      "Lightning Talk #3 - Career Development: Mastering the Art of Personal Branding ",
     imageUrl: "https://i.pravatar.cc/150?img=50",
     time: "10:30 AM",
-    place: "Conference Room A",
-    description: "Exciting lightning talk on a trending topic!",
-    type: "regular",
-    speaker: "GM",
-  },
-  {
-    title: "Lightning Talk #2",
-    imageUrl: "https://i.pravatar.cc/150?img=50",
-    time: "11:00 AM",
     place: "Conference Room B",
-    description: "Another exciting lightning talk, this time by IBM!",
+    description:
+      "Uncover the power of personal branding with Kanika Mohan from Dell Technologies. Learn how to craft a unique brand that captivates employers and propels your career to new heights.",
     type: "regular",
-    speaker: "IBM",
+    speaker: " Kanika Mohan @ Dell Technologies",
   },
 
   {
-    title: "Lightning Talk #3",
-    imageUrl: "https://i.pravatar.cc/150?img=50",
+    title: "Lightning Talk #4 - Financial Wellness for Women",
+    imageUrl:
+      "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
+    time: "11:00 AM",
+    place: "Conference Room C",
+    description:
+      "Join Nana Oh from New York Life Insurance Company for an empowering discussion on Financial Wellness for Women. Gain valuable insights and strategies to take control of your financial future.",
+    type: "regular",
+    speaker: "Eunkyung (Nana) Oh @ New York Life Insurance Company",
+  },
+  {
+    title:
+      "Lightning Talk #5 - Empower Your Journey: Unleashing Potential & Embracing Possibilities ",
+    imageUrl:
+      "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
     time: "11:30 AM",
     place: "Conference Room C",
-    description: "Exciting lightning talk on a new technology!",
+    description:
+      "Embark on a transformative journey with Krissie Knight from Apple Developer Academy. Discover the keys to unlocking your full potential and embracing a future filled with endless possibilities.",
     type: "regular",
-    speaker: "TBD",
-  },
-  {
-    title: "Lightning Talk #4",
-    imageUrl:
-      "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
-    time: "11:30 AM",
-    place: "Conference Room C",
-    description: "Exciting lightning talk on a new technology!",
-    type: "regular",
-    speaker: "TBD",
-  },
-
-  {
-    title: "Afternoon Keynote",
-    imageUrl:
-      "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
-    time: "1:00 PM",
-    place: "Main Hall",
-    description: "Join DTE Energy for an inspiring afternoon keynote!",
-    type: "regular",
-    speaker: "DTE Energy",
-  },
-  {
-    title: "Panel Discussion",
-    imageUrl:
-      "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
-    time: "2:00 PM",
-    place: "Main Hall",
-    description: "Engage in a lively panel discussion with industry experts!",
-    type: "panel",
-    moderator: "NSBE, SHPE and SWE Detroit Professional Chapters",
-    panelists: "John Doe, Jane Smith, Alex Johnson, Emily Brown",
-  },
-  {
-    title: "Closing Remarks & Giveaways",
-    imageUrl:
-      "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
-    time: "3:30 PM",
-    place: "Main Hall",
-    description: "Join us for closing remarks and exciting giveaways!",
-    type: "regular",
-    speaker: "NSBE, SHPE and SWE Detroit Professional Chapters",
+    speaker: "Krissie Knight @ Apple Developer Academy",
   },
 ];
 
