d776ae5a67e82e667a39747d178dcb4850d9d0fc
khanabdu25 <khanabdu@umich.edu>
2024-03-17 00:11:18 -0400

    padd loc childs

--- iwd-app/src/components/location-section/location.jsx
+++ iwd-app/src/components/location-section/location.jsx
@@ -8,7 +8,7 @@ function Location() {
     /* app-section style is necessary, please do not delete */
     <div className="app-section location-div">
       <div class="flex flex-col mx-20 my-5 md:flex-row md:my-20 md:mx-5">
-        <div class="flex flex-row items-center justify-start md:flex-1">
+        <div class="flex flex-row items-center justify-start m-2 md:flex-1">
           <img
             src={WHENPATH}
             className="w-1/2 md:w-1/4"
@@ -19,7 +19,7 @@ function Location() {
             <p className="text-description">Sat, Mar 30, 8:30 AM (EDT)</p>
           </div>
         </div>
-        <div class="flex flex-row items-center justify-center md:flex-1">
+        <div class="flex flex-row items-center justify-center m-2 md:flex-1">
           <img
             src={WHEREPATH}
             class="w-1/2 md:w-1/4"
@@ -32,7 +32,7 @@ function Location() {
             </p>
           </div>
         </div>
-        <div class="flex flex-row items-center justify-center md:flex-1">
+        <div class="flex flex-row items-center justify-center m-2 md:flex-1">
           <iframe
             title="yuh"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2949.096872823791!2d-83.0570689236836!3d42.34045797119474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x883b2d326fc04bed%3A0x8608d20b7f2548b7!2sGoogle%20Detroit!5e0!3m2!1sen!2sus!4v1709013405727!5m2!1sen!2sus"
