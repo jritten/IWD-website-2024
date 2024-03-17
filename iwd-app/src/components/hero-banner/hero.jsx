72e59278e9f2ff4dd30018de0740f14a9533f5a9
maryamtello1 <maryamt@umich.edu>
2024-03-17 11:14:58 -0400

    refractored women img into different column

--- iwd-app/src/components/hero-banner/hero.jsx
+++ iwd-app/src/components/hero-banner/hero.jsx
@@ -9,6 +9,9 @@ import dateAnimation from "../../assets/hero/date-animation.gif";
 function Hero() {
   return (
     <div className="app-section hero-banner">
+      <div className="background-div">
+        <img id="background-img" src={background}></img>
+      </div>
       <div className="top-row">
         <div className="title-div">
           <div className="heading-div">
@@ -26,12 +29,13 @@ function Hero() {
           </p>
           <btn id="tickets-btn">Get Tickets</btn>
         </div>
-        <img id="woman-hero" src={heroGirl}></img>
-      </div>
-      <img id="background-img" src={background}></img>
-      <div className="date-img-div">
-        <img id="date-animation" src={dateAnimation}></img>
-        <img id="date-img" src={date}></img>
+        <div className="woman-date-column">
+          <img id="woman-hero" src={heroGirl}></img>
+          <div className="date-img-div">
+            <img id="date-animation" src={dateAnimation}></img>
+            <img id="date-img" src={date}></img>
+          </div>
+        </div>
       </div>
     </div>
   );
