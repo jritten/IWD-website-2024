f56d0bd5775ff94be5b0e608e20fcdaf152f793b
maryamtello1 <maryamt@umich.edu>
2024-03-18 13:27:06 -0400

    mobile view

--- iwd-app/src/components/organizers-section/organizers-list.js
+++ iwd-app/src/components/organizers-section/organizers-list.js
@@ -5,18 +5,6 @@ const organizersData = [
     name: "Jenna Ritten",
     position: "",
   },
-  {
-    imageurl:
-      "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
-    name: "Jenna Ritten",
-    position: "",
-  },
-  {
-    imageurl:
-      "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
-    name: "Jenna Ritten",
-    position: "",
-  },
 ];
 
 export default organizersData;
