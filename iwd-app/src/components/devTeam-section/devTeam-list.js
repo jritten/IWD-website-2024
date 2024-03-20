46725cbe05594fabe7296a1a310e47a9cdf25835
MoeAbboud <mohamadabboud200@outlook.com>
2024-03-20 17:16:09 -0400

    added new members to devTeam section

--- iwd-app/src/components/devTeam-section/devTeam-list.js
+++ iwd-app/src/components/devTeam-section/devTeam-list.js
@@ -4,6 +4,8 @@ import JesusOrozco from "../../assets/headshots/JesusOrozco.jpg"
 import RazanShams from "../../assets/headshots/RazanShams.jpg"
 import AbdulKhan from "../../assets/headshots/AbdulKhan.jpg"
 import AnikaRaisa from "../../assets/headshots/AnikaRaisa.jpg"
+import MouhamedSalma from "../../assets/headshots/MouhamedSalma.jpg"
+import GayathriChava from "../../assets/headshots/GayathriChava.jpg"
 
 const devTeamData = [
     {
@@ -43,16 +45,16 @@ const devTeamData = [
         status: "Graduate Student",
     },
     {
-        imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
-        name: "",
-        linkedIn: "https://www.linkedin.com/in/moeabboud/",
-        status: "",
+        imageurl: MouhamedSalma,
+        name: "Mouhamed Salma",
+        linkedIn: "https://www.linkedin.com/in/mouhamed-salma/",
+        status: "Senior",
     },
     {
-        imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
-        name: "",
-        linkedIn: "https://www.linkedin.com/in/moeabboud/",
-        status: "",
+        imageurl: GayathriChava,
+        name: "Gayathri Chava",
+        linkedIn: "https://www.linkedin.com/in/cgayathri21f?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+        status: "Junior",
     },
     {
         imageurl: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg",
