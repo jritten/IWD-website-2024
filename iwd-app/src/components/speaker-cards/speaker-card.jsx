d995a649724836f41ee7f99629c2514bb022af66
Mo Chami <moeelchami@gmail.com>
2024-03-21 12:49:42 -0400

    Switched the job title and company positions and made blue background follow text element

--- iwd-app/src/components/speaker-cards/speaker-card.jsx
+++ iwd-app/src/components/speaker-cards/speaker-card.jsx
@@ -10,10 +10,10 @@ function SpeakerCard({imageurl, name, position, company, description}) {
        <div className='speaker-name'>
           {name || "No Name"}
        </div>
-       <div className='speaker-position'>
+       <div className='speaker-company'>
           {position || "No Position"}
        </div>
-       <div className='speaker-company'>
+       <div className='speaker-position'>
           {company || "No Company"}
        </div>
        <div className='speaker-description'>
