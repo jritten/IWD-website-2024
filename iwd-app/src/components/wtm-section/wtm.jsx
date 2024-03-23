89ff3e56d5ee6f5f7e6c179c0f15b038e4c27dd8
maryamtello1 <maryamt@umich.edu>
2024-03-22 22:12:34 -0400

    made wtm heading look better

--- iwd-app/src/components/wtm-section/wtm.jsx
+++ iwd-app/src/components/wtm-section/wtm.jsx
@@ -5,13 +5,15 @@ import Carousel from "../carousel/carousel";
 function WomenTechmakers() {
   return (
     <div className="app-section wtm-section">
-      <h2>Meet the Women Techmakers</h2>
+      <h2>
+        Meet the <span className="wtm-word">Women Techmakers</span>
+      </h2>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur.
+        velit esse cillum.
       </p>
 
       <Carousel />
