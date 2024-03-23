f1d3e5ad5181232e020a537bc1c38290438eea02
Heba Sayed <hebasayed1603@gmail.com>
2024-03-23 00:52:45 -0400

    added flowers to the speakers

--- iwd-app/src/components/speakers-section/speakers-list.js
+++ iwd-app/src/components/speakers-section/speakers-list.js
@@ -5,53 +5,68 @@ import AlissaSevrioukova from "../../assets/speaker-imgs/AlissaSevrioukova.jpeg"
 import EunkyungOh from "../../assets/speaker-imgs/EunkyungOh.jpg";
 import IdaByrdHill from "../../assets/speaker-imgs/IdaByrdHill.jpeg";
 
-
+import Bell from "../../assets/flowers/Bell.png";
+import Hydrangea from "../../assets/Hydrangea.png";
+import blueHydrangea from "../../assets/flowers/blueHydra.png";
+import Dandelion from "../../assets/flowers/Dandelion.png";
+import osteoPink from "../../assets/flowers/osteoPink.png";
+import rose from "../../assets/flowers/Soft red flower.png";
 
 const speakersData = [
-    {
-        imageurl: AlissaSevrioukova,
-        name: "Alissa Sevrioukova",
-        position: "Chief of Staff",
-        company: "DTE Energy",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis ut lectus non sollicitudin.",
-    },
-    {
-        imageurl: SeungjuChoi,
-        name: "Seungju Choi",
-        position: "UX Researcher",
-        company: "UofM, Human System Integration Lab",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis ut lectus non sollicitudin.",
-    },
-    {
-        imageurl: KanikaMohan,
-        name: "Kanika Mohan",
-        position: "Product Manager Intern",
-        company: "Dell Technologies",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis ut lectus non sollicitudin.",
-    },
-    {
-        imageurl: EunkyungOh,
-        name: "Eunkyung (Nana) Oh",
-        position: "Financial Services Agent",
-        company: "New York Life Insurance Company",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis ut lectus non sollicitudin.",
-    },
-    {
-        imageurl: KrissieKnight,
-        name: "Krissie Knight",
-        position: "UX/UI Designer",
-        company: "Apple Developer Academy",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis ut lectus non sollicitudin.",
-    },
-    {
-        imageurl: IdaByrdHill,
-        name: "Ida Byrd-Hill",
-        position: "CEO & Founder",
-        company: "Automation Workz",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis ut lectus non sollicitudin.",
-    }
-    
-]
-
+  {
+    imageurl: AlissaSevrioukova,
+    name: "Alissa Sevrioukova",
+    position: "Chief of Staff",
+    company: "DTE Energy",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis ut lectus non sollicitudin.",
+    flower: Bell,
+  },
+  {
+    imageurl: SeungjuChoi,
+    name: "Seungju Choi",
+    position: "UX Researcher",
+    company: "UofM, Human System Integration Lab",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis ut lectus non sollicitudin.",
+    flower: Hydrangea,
+  },
+  {
+    imageurl: KanikaMohan,
+    name: "Kanika Mohan",
+    position: "Product Manager Intern",
+    company: "Dell Technologies",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis ut lectus non sollicitudin.",
+    flower: blueHydrangea,
+  },
+  {
+    imageurl: EunkyungOh,
+    name: "Eunkyung (Nana) Oh",
+    position: "Financial Services Agent",
+    company: "New York Life Insurance Company",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis ut lectus non sollicitudin.",
+    flower: Dandelion,
+  },
+  {
+    imageurl: KrissieKnight,
+    name: "Krissie Knight",
+    position: "UX/UI Designer",
+    company: "Apple Developer Academy",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis ut lectus non sollicitudin.",
+    flower: osteoPink,
+  },
+  {
+    imageurl: IdaByrdHill,
+    name: "Ida Byrd-Hill",
+    position: "CEO & Founder",
+    company: "Automation Workz",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis ut lectus non sollicitudin.",
+    flower: rose,
+  },
+];
 
 export default speakersData;
