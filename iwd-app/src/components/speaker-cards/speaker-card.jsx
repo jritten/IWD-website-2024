964c11370943f151427e7a8a9458725978027f05
maryamtello1 <maryamt@umich.edu>
2024-03-27 18:28:19 -0400

    made width responsive for smaller sizes

--- iwd-app/src/components/speaker-cards/speaker-card.jsx
+++ iwd-app/src/components/speaker-cards/speaker-card.jsx
@@ -31,22 +31,22 @@ function SpeakerCard({
 
   return (
     <div
-      className="speaker-card min-h-[330px] md:m-10 mx-4 my-6 md:w-[310px] md:min-h-[410px] w-[170px] min-h-[3rem] lg:w-[310px] lg:min-h-[410px]"
+      className="speaker-card min-h-[330px] sm:m-10 mx-4 my-6 sm:w-[310px] sm:min-h-[410px] w-5/6 lg:w-[310px] lg:min-h-[410px]"
       onClick={handleCardClick}
       style={{
         cursor: description ? (showModal ? "default" : "pointer") : "default",
       }}
     >
-      <div className="speaker-image rounded-[0px] w-[130px] h-[130px] md:w-[172px] md:h-[172px]">
+      <div className="speaker-image rounded-[0px] w-[130px] h-[130px] sm:w-[172px] sm:h-[172px]">
         <img src={imageurl} alt="Speaker image" />
       </div>
-      <div className="speaker-name text:base leading-5 md:text-3xl md:leading-10">
+      <div className="speaker-name text:base leading-5 sm:text-3xl sm:leading-10">
         {name || "No Name"}
       </div>
-      <div className="speaker-position text-sm leading-4  my-3 sm:text-lg  md:leading-10">
+      <div className="speaker-position text-sm leading-4  my-3 sm:text-lg  sm:leading-10">
         {position || "No Position"}
       </div>
-      <div className="speaker-company text-xs leading-4  my-3 sm:text-lg  md:leading-10">
+      <div className="speaker-company text-xs leading-4  my-3 sm:text-lg  sm:leading-10">
         {company || "No Company"}
       </div>
       {showModal && description && (
