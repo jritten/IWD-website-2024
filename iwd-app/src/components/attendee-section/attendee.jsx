efaf96091050fe84459a8c16b57bf09d9f34e700
Anika Chowdhury <raisaanika@meh.local>
2024-03-27 12:38:14 -0400

    need to fix hover and nav link

--- iwd-app/src/components/attendee-section/attendee.jsx
+++ iwd-app/src/components/attendee-section/attendee.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Marquee from 'react-fast-marquee';
+import "../../globalStyles.css";
 import './attendee.css';
 
 function Attendees() {
   const [imagePaths, setImagePaths] = useState([]);
+  const [isHovered, setIsHovered] = useState(false);
 
   useEffect(() => {
     const importImages = async () => {
@@ -21,16 +23,27 @@ function Attendees() {
     importImages();
   }, []);
 
+  const handleHover = () => {
+    setIsHovered(!isHovered);
+  };
+
   return (
-    <div className="app-section attendee-div">
-      <div className="attendee-title">Attendees</div>
-      <Marquee direction="left" speed={100}>
+    <div 
+      className="app-section attendee-div" 
+      id="attendee-section" 
+      onMouseEnter={handleHover} 
+      onMouseLeave={handleHover}
+    >
+      <div className="section-title">Attendees</div>
+
+      <Marquee direction="left" speed={100} className="marquee-container" pauseOnHover={!isHovered}>
         {imagePaths.map((path, index) => (
           <div key={index} className="attendee-image-wrapper">
             <img src={path} alt={`Attendee ${index + 1}`} className="attendee-image" />
           </div>
         ))}
       </Marquee>
+      
     </div>
   );
 }
