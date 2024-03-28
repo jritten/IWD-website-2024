1cad9c400708d7efb418933294618ef7373214db
maryamtello1 <maryamt@umich.edu>
2024-03-27 20:31:33 -0400

    reverted speaker cards

--- iwd-app/src/components/speaker-cards/speaker-card.jsx
+++ iwd-app/src/components/speaker-cards/speaker-card.jsx
@@ -10,8 +10,10 @@ function SpeakerCard({
   description,
   flower,
 }) {
+  // state to manage the visibility of the modal
   const [showModal, setShowModal] = useState(false);
 
+  // Functions to open/close the modal
   const openModal = () => {
     setShowModal(true);
   };
@@ -19,10 +21,14 @@ function SpeakerCard({
     setShowModal(false);
   };
 
+  // Function to handle clicking the speaker card
   const handleCardClick = (event) => {
+    // Check if the close button is clicked
     if (event.target.classList.contains("close-button")) {
+      // Prevent the event from bubbling to the parent div
       event.stopPropagation();
     } else {
+      // Open the modal if the close button is not clicked and description is available
       if (description) {
         openModal();
       }
@@ -31,24 +37,19 @@ function SpeakerCard({
 
   return (
     <div
-      className="speaker-card px-3 pb-2 min-h-[300px] sm:m-10 sm:p-3 mx-4 my-6 sm:w-[310px] sm:min-h-[410px] w-5/6 lg:w-[310px] lg:min-h-[410px]"
+      className="speaker-card w-[210px] min-h-[3rem] lg:w-[310px] lg:min-h-[410px] "
       onClick={handleCardClick}
       style={{
         cursor: description ? (showModal ? "default" : "pointer") : "default",
       }}
     >
-      <div className="speaker-image rounded-[0px] w-[130px] h-[130px] sm:w-[172px] sm:h-[172px]">
+      <div className="speaker-image rounded-[0px] w-[100px] h-[100px] lg:w-[172px] lg:h-[172px]">
         <img src={imageurl} alt="Speaker image" />
       </div>
-      <div className="speaker-name text:base leading-5 sm:text-3xl sm:leading-10">
-        {name || "No Name"}
-      </div>
-      <div className="speaker-position text-sm leading-4  my-3 sm:text-lg  sm:leading-10">
-        {position || "No Position"}
-      </div>
-      <div className="speaker-company text-xs leading-4  my-3 sm:text-lg  sm:leading-5">
-        {company || "No Company"}
-      </div>
+      <div className="speaker-name">{name || "No Name"}</div>
+      <div className="speaker-position my-3">{position || "No Position"}</div>
+      <div className="speaker-company">{company || "No Company"}</div>
+      {/* render the pop-up card if showModal state is true and description is available */}
       {showModal && description && (
         <SpeakerPopupCard
           imageurl={imageurl}
