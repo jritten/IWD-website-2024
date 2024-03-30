333dd964b226c6a54dc90a4528f12d4edd404dd6
RaisaAnika <araisa@umich.edu>
2024-03-29 21:28:42 -0400

    full agenda update

--- iwd-app/src/components/sessions-section/full-agenda.js
+++ iwd-app/src/components/sessions-section/full-agenda.js
@@ -37,7 +37,7 @@ const agendaCards = [
     title: "Lightning Talk #1 - Tech, Design & AI",
     imageUrl: SeungjuChoi,
     time: "9:30 AM",
-    place: "Conference Room A",
+    place: "G.O. 9th floor auditorium",
     description:
       "Join Seungju Choi from the University of Michigan's Human System Integration Lab for an insightful discussion on drivers' performance, distraction, and satisfaction.",
     type: "regular",
@@ -49,7 +49,7 @@ const agendaCards = [
     title: "Lightning Talk #2 - Detroit Artistry: Women Artists Making Waves",
     imageUrl: people,
     time: "10:00 AM",
-    place: "Conference Room B",
+    place: "G.O. 9th floor auditorium",
     description:
       "Immerse yourself in the soulful creativity of Detroit. Discover the heartbeat of a city known for its vibrant cultural tapestry and artistic flair.",
     type: "regular",
@@ -61,7 +61,7 @@ const agendaCards = [
       "Lightning Talk #3 - Career Development: Mastering the Art of Personal Branding ",
     imageUrl: KanikaMohan,
     time: "10:30 AM",
-    place: "Conference Room B",
+    place: "G.O. 9th floor auditorium",
     description:
       "Uncover the power of personal branding with Kanika Mohan from Dell Technologies. Learn how to craft a unique brand that captivates employers and propels your career to new heights.",
     type: "regular",
@@ -73,7 +73,7 @@ const agendaCards = [
     title: "Lightning Talk #4 - Financial Wellness for Women",
     imageUrl: EunkyungOh,
     time: "11:00 AM",
-    place: "Conference Room C",
+    place: "G.O. 9th floor auditorium",
     description:
       "Join Nana Oh from New York Life Insurance Company for an empowering discussion on Financial Wellness for Women. Gain valuable insights and strategies to take control of your financial future.",
     type: "regular",
@@ -85,7 +85,7 @@ const agendaCards = [
       "Lightning Talk #5 - Empower Your Journey: Unleashing Potential & Embracing Possibilities ",
     imageUrl: KrissieKnight,
     time: "11:30 AM",
-    place: "Conference Room C",
+    place: "G.O. 9th floor auditorium",
     description:
       "Embark on a transformative journey with Krissie Knight from Apple Developer Academy. Discover the keys to unlocking your full potential and embracing a future filled with endless possibilities.",
     type: "regular",
