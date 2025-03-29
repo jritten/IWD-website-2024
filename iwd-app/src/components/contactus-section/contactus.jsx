c148e0c034f2526b70e29c263a2b1e7cae310108
Divyalakshmi Varadha Rajan Prem Sudha <69458308+Divyatech-hub@users.noreply.github.com>
2025-03-29 07:23:59 -0400

    Update contactus.jsx

--- iwd-app/src/components/contactus-section/contactus.jsx
+++ iwd-app/src/components/contactus-section/contactus.jsx
@@ -5,36 +5,34 @@
   function ContactUs() {
     return (
       <footer id="contactus-div" className="app-section contactus-div md:h-30">
-  <div className="footer-content">
+  <div className="footer-inner">
     <div className="footer-logo">
-      <img src={wtmlogo} alt="WTM Logo" />
+      <img src={logo} alt="Compass Logo" />
     </div>
 
-    <div className="flex flex-col items-center footer-main">
+    <div className="text-column">
       <div className="section-title">Contact Us</div>
 
       <div className="mt-2 flex space-x-4">
-        <div className="mt-2 flex space-x-4">
-          {/* LinkedIn Icon */}
-          <a 
-            href="https://bit.ly/compass-linkedin"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mb-2 inline-block rounded-full bg-white p-3 shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
-          >
-            {/* SVG */}
-          </a>
+        {/* LinkedIn */}
+        <a 
+          href="https://bit.ly/compass-linkedin"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mb-2 inline-block rounded-full bg-white p-3 shadow-md transition duration-150 ease-in-out hover:shadow-lg"
+        >
+          {/* LinkedIn SVG */}
+        </a>
 
-          {/* Instagram Icon */}
-          <a 
-            href="https://bit.ly/compassdetroit-instagram"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mb-2 inline-block rounded-full bg-white p-3 shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
-          >
-            {/* SVG */}
-          </a>
-        </div>
+        {/* Instagram */}
+        <a 
+          href="https://bit.ly/compassdetroit-instagram"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mb-2 inline-block rounded-full bg-white p-3 shadow-md transition duration-150 ease-in-out hover:shadow-lg"
+        >
+          {/* Instagram SVG */}
+        </a>
       </div>
     </div>
   </div>
